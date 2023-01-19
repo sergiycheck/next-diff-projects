c19b542289253d677d5f232d5bfbac2377cb7a4c
serhii <sergiychech@gmail.com>
2023-01-19 18:05:13 +0200

    joshwcomeau css reset styles

---
+++ default-next-js-project-to-try-things/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <p>
+        This paragraph has a 1.5x ratio for line-height, and it feels pretty
+        good, right? I think this text is legible and pleasant.
+      </p>
+      <h1>But it's a bit much on headings!</h1>
+    </main>
+  );
+}
