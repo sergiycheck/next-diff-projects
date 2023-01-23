1a28a5ebf3d11c0bcf6cc3f4760d6f4a7f4f8232
serhii <sergiychech@gmail.com>
2023-01-23 19:04:49 +0200

    section under header section

--- nextjs-with-tailwind/landing-page/src/app/components/home/HomePage.tsx
+++ nextjs-with-tailwind/landing-page/src/app/components/home/HomePage.tsx
@@ -12,17 +12,17 @@ import FilledPlot from '/public/media/section-1-imgs/filled-plot.png';
 import HouseImage from '/public/media/section-1-imgs/house.jpeg';
 import HouseIconImage from '/public/media/section-1-imgs/house-icon.png';
 import MoneyCoinsImage from '/public/media/section-1-imgs/money-coins.png';
-import { H1Heading, H3Heading } from '../common/headings/headings';
+import { H1Heading, H3Heading, H4Heading } from '../common/headings/headings';
 import { PEl, Text } from '../common/textsContainers/textContainers';
+
+import LightingLampImage from '/public/media/section-2-imgs/lighting_lamp.png';
+import PapersServiceImage from '/public/media/section-2-imgs/papers_service.png';
+import PlotPerformanceImage from '/public/media/section-2-imgs/plot_performance.png';
 
 export const HomePage = () => {
   return (
     <>
       <div className="w-full bg-zinc-100 relative">
-        <div className="lg:container lg:mx-auto mx-auto px-4 pt-20 sm:pt-32">
-          <SectionFirst />
-        </div>
-
         <div
           className="
           absolute lg:bg-black
@@ -34,16 +34,15 @@ export const HomePage = () => {
           absolute lg:bg-black
           left-[-50px] top-[550px] half_circle_right"
         ></div>
+
+        <div className="lg:container lg:mx-auto mx-auto px-4 pt-20 sm:pt-32">
+          <SectionFirst />
+        </div>
       </div>
 
       <div className="bg-white">
         <div className="lg:container lg:mx-auto mx-auto px-4">
-          <H3Heading>
-            <Text>
-              The professional Consultation Platform For Your Real Estate and
-              Finance
-            </Text>
-          </H3Heading>
+          <SectionSecond />
         </div>
       </div>
     </>
@@ -250,6 +249,102 @@ export const BlackSmallRoundedImage = (props: BlackSmallRoundedImageProps) => {
       shadow-2xl"
     >
       <Image {...props} />
+    </div>
+  );
+};
+
+export const SectionSecond = () => {
+  return (
+    <>
+      <H3Heading>
+        <Text>
+          The professional Consultation Platform For Your Real Estate and
+          Finance
+        </Text>
+      </H3Heading>
+
+      <div className="flex gap-4 flex-col sm:flex-row mt-16">
+        <CliableSectionOfElements />
+      </div>
+    </>
+  );
+};
+
+export const CliableSectionOfElements = () => {
+  const arrOfdataToPass: WhiteShadowedBlockWithImage[] = [
+    {
+      Image: (
+        <Image
+          className="w-[80px] h-auto"
+          src={LightingLampImage}
+          alt="lamp icon"
+        ></Image>
+      ),
+      title: 'We innovate',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+      voluptatem qui optio animi`,
+    },
+    {
+      Image: (
+        <Image
+          className="w-[80px] h-auto"
+          src={PlotPerformanceImage}
+          alt="papers icon"
+        ></Image>
+      ),
+      title: 'Performance',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+      voluptatem qui optio animi`,
+    },
+    {
+      Image: (
+        <Image
+          className="w-[80px] h-auto"
+          src={PapersServiceImage}
+          alt="papers icon"
+        ></Image>
+      ),
+      title: 'A Full Service',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+      voluptatem qui optio animi`,
+    },
+  ];
+
+  return (
+    <>
+      {arrOfdataToPass.map((el, index) => (
+        <WhiteShadowedBlockWithImage
+          key={index}
+          Image={el.Image}
+          title={el.title}
+          text={el.text}
+        />
+      ))}
+    </>
+  );
+};
+
+type WhiteShadowedBlockWithImage = {
+  Image: JSX.Element;
+  title: string;
+  text: string;
+};
+export const WhiteShadowedBlockWithImage = (
+  props: WhiteShadowedBlockWithImage
+) => {
+  return (
+    <div className="drop-shadow-2xl flex basis-1/3 bg-white rounded-lg p-3">
+      <div className="basis-2/6 flex justify-center items-center">
+        {props.Image}
+      </div>
+      <div className="basis-4/6 flex flex-col gap-4">
+        <PEl className="font-bold text-lg">
+          <Text>{props.title}</Text>
+        </PEl>
+        <PEl className="text-neutral-500">
+          <Text>{props.text}</Text>
+        </PEl>
+      </div>
     </div>
   );
 };
