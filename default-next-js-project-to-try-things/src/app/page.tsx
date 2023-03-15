62590a12f6d9796207f4a77e5f3912755ddd4d07
serhii <sergiychech@gmail.com>
2023-03-15 15:03:47 +0200

    add nft image

--- default-next-js-project-to-try-things/src/app/page.tsx
+++ default-next-js-project-to-try-things/src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Text } from "./shared";
 import s from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={s.container}>
       <Text className={s.customText}>Some text</Text>
+
+      <Text>Image from obtkit marketplace</Text>
+      <Image
+        src="https://assets.objkt.media/file/assets-003/QmdXYBm9kkHDLHv1LqQYMaHH5Yu2j74DEFh6ioLpcyPxc9/artifact"
+        height={200}
+        width={200}
+        alt="nft 1"
+      />
     </div>
   );
 }
