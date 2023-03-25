34af5016c474193c659d1f8be0008758257df708
serhii <sergiychech@gmail.com>
2023-03-25 22:45:39 +0200

    configured slowly fast refresh with docker

--- authentication/authjs/multiple-credential-providers-docker/start_dev.sh
+++ authentication/authjs/multiple-credential-providers-docker/start_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # docker compose -f docker-compose-dev.yml up --build
-docker compose -f docker-compose-dev.yml down --volumes
+docker compose -f docker-compose-dev.yml up 
+# docker compose -f docker-compose-dev.yml down --volumes
